f49fd6e822efb7f54b074b2edbe9e9a2d75c8324
Trison Nguyen <trison.ng@gmail.com>
2017-03-02 20:05:37 -0800

    draggable card

--- js/vanilla-script.js
+++ js/vanilla-script.js
@@ -1,42 +1,39 @@
-
-
-
+//fade in card
 window.onload = fade;
 function fade(){
 	var card = document.getElementById("card");
 	card.style.transition = "opacity 0.6s";
 	card.style.opacity = "1";
+
+	var underCard = document.getElementById("under-card");
+	underCard.style.transition = "opacity 3s";
+	underCard.style.opacity = "1";
 }
 
 /*** KONAMI CODE ***/
 var allowedKeys = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  65: 'a',
-  66: 'b'
+ 	37: 'left',
+ 	38: 'up',
+ 	39: 'right',
+	40: 'down',
+	65: 'a',
+	66: 'b'
 };
 
-// Konami Code sequence
 var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-
 var konamiCodePosition = 0;
 
-// add keydown event listener
 document.addEventListener('keydown', function(e) {
-  var key = allowedKeys[e.keyCode];
-  var requiredKey = konamiCode[konamiCodePosition];
+	var key = allowedKeys[e.keyCode];
+	var requiredKey = konamiCode[konamiCodePosition];
 
-  // compare the key with the required key
-  if (key == requiredKey) {
-
-    konamiCodePosition++;
-
-    if (konamiCodePosition == konamiCode.length)
-      activateCheat();
-  } else
-    konamiCodePosition = 0;
+	// compare entered  key with required key
+	if (key == requiredKey) {
+		konamiCodePosition++;
+		if (konamiCodePosition == konamiCode.length)
+			activateCheat();
+	}
+	else konamiCodePosition = 0;
 });
 
 function activateCheat() {
